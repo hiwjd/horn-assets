f5a78e2da16fc69f27444526ac9d360d46fb0a47
hiwjd <self@hiwjd.com>
2016-11-21 08:39:51 +0800

    在线访客栏目

--- src/components/Visitor.vue
+++ src/components/Visitor.vue
@@ -1,3 +1,36 @@
 <template>
-    <div>Visitor</div>
+    <div>
+        Visitor
+        <div>
+            <ul>
+                <li v-for="user in users">{{user.uid}} {{user.os}} {{user.browser}} <a target="_blank" :href="user.url">{{user.title}}</a></li>
+            </ul>
+        </div>
+    </div>
 </template>
+
+<script>
+    export default {
+        created () {
+            var _self = this;
+            function fetch() {
+                HORN.GetOnlineUserList(function(r) {
+                    for(var i=0; i<r.data.length; i++) {
+                        _self.$store.dispatch("addUser", {user: r.data[i]});
+                    }
+                }, function(err) {
+                    alert(err);
+                });
+            }
+
+            fetch();
+
+            setInterval(fetch, 5000);
+        },
+        computed: {
+            users() {
+                return this.$store.state.users;
+            }
+        }
+    }
+</script>
